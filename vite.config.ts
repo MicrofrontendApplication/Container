cd0a5b3344bd61b7ff37642601d9a890e4d6712c
Nishant <nishant.prabhu@techdenali.com>
2025-07-23 11:50:47 +0530

    changed the url of plp and pdp

--- vite.config.ts
+++ vite.config.ts
@@ -12,12 +12,12 @@ export default defineConfig({
         // plp: 'https://plppage.onrender.com/assets/remoteEntry.js',
         // pdp: 'https://pdppage.onrender.com/assets/remoteEntry.js',
 
-        plp: 'http://localhost:5004/assets/remoteEntry.js',
-        pdp: 'http://localhost:5005/assets/remoteEntry.js',
+        // plp: 'http://localhost:5004/assets/remoteEntry.js',
+        // pdp: 'http://localhost:5005/assets/remoteEntry.js',
 
 
-        // plp: 'https://plppage-blue.vercel.app/assets/remoteEntry.js',
-        // pdp: 'https://pdppage.vercel.app/assets/remoteEntry.js',
+        plp: 'https://plppage-ruddy.vercel.app/assets/remoteEntry.js',
+        pdp: 'https://pdppage.vercel.app/assets/remoteEntry.js',
       },
    shared: ['react', 'react-dom', 'react-router-dom'],
     }),
